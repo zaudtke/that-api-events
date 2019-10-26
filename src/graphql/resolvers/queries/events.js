d88f1214a538c36ba44b788113b2978458c49972
Clark Sell <clark.sell@csell.net>
2019-10-25 21:54:55 -0500

    and now you can get an event by its id

--- src/graphql/resolvers/queries/events.js
+++ src/graphql/resolvers/queries/events.js
@@ -1,6 +1,9 @@
 import eventStore from '../../../dataSources/cloudFirestore/event';
 
 const resolvers = {
+  event: async (parent, { id }, { dataSources }) =>
+    eventStore(dataSources.firestore).get(id),
+
   events: async (parent, args, { dataSources }) =>
     eventStore(dataSources.firestore).getAll(),
 };
