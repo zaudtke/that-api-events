7e823a27271dfa420215c47f34de4ebd7df7f097
Clark Sell <clark.sell@csell.net>
2019-12-07 20:28:12 -0600

    adds pino, and resolves venues

--- src/graphql/resolvers/queries/event.js
+++ src/graphql/resolvers/queries/event.js
@@ -1,17 +1,27 @@
 import eventStore from '../../../dataSources/cloudFirestore/event';
+import venueStore from '../../../dataSources/cloudFirestore/venue';
+
 import notificationResolver from './notification';
 
 const resolvers = {
-  event: async (parent, { id }, { dataSources }) =>
-    eventStore(dataSources.firestore).get(id),
+  event: async (parent, { id }, { dataSources: { firestore, logger } }) =>
+    eventStore(firestore, logger).get(id),
 
-  events: async (parent, args, { dataSources }) =>
-    eventStore(dataSources.firestore).getAll(),
+  events: async (parent, args, { dataSources: { firestore, logger } }) =>
+    eventStore(firestore, logger).getAll(),
 };
 
 export const fieldResolvers = {
   Event: {
     notifications: notificationResolver.notifications,
+    venues: async (
+      { venues },
+      args,
+      { dataSources: { firestore, logger } },
+    ) => {
+      logger.debug('Event:venues called');
+      return venueStore(firestore, logger).findByIds(venues);
+    },
   },
 };
 
