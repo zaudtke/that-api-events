2de85a291359527c9f7a46b8dec330c848f753d7
Brett Slaski <brett@thatconference.com>
2020-06-08 14:14:52 -0500

    change to that-api date scalar. update packages

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,12 +1,12 @@
 import { resolvers as graphScalars } from 'graphql-scalars';
+import { graph } from '@thatconference/api';
 
-import customScalars from './scalars/date';
 import queries, { fieldResolvers as qFieldResolvers } from './queries';
 import mutations, { fieldResolvers as mFieldResolvers } from './mutations';
 
 const createServer = {
   ...graphScalars,
-  ...customScalars,
+  ...graph.scalars.date,
 
   ...qFieldResolvers,
   ...mFieldResolvers,
